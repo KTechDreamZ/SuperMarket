f580f7c30f80237c2276f58db0d0c617aaf66de2
Abiyassyed <120255599+Abiyassyed@users.noreply.github.com>
2025-04-22 17:40:25 +0530

    dim.store.py

--- dim.store.py
+++ dim.store.py
@@ -6,7 +6,6 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from sqlalchemy import create_engine
 from datetime import datetime, timedelta
 
-# Constants
 POSTGRES_CONN_ID = "project_connection"
 SOURCE_TABLE = "sales_transformed_dag"
 DIM_STORE_TABLE = "dim_store"
@@ -35,7 +34,6 @@ def load_dim_store():
     engine = create_db_engine()
     create_dim_store_table(engine)
 
-    # Extract all product data (no DISTINCT)
     query = f"""
         SELECT 
             "Store ID", 
@@ -47,15 +45,12 @@ def load_dim_store():
     """
     df = pd.read_sql(query, engine)
 
-    # Add load date to all rows
     df["Load Date"] = datetime.today().date()
 
-    # Load all rows (no upsert, just append)
     df.to_sql(DIM_STORE_TABLE, engine, if_exists="append", index=False)
 
     logging.info("✅ dim_store table loaded with all rows (duplicates allowed).")
 
-# DAG Definition
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2025, 4, 4),
